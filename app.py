7f18a124b5f083c63c85b02d6829745d816df4dd
root <root@ip-172-31-24-134.cn-north-1.compute.internal>
2024-10-23 05:40:40 +0000

    add app

---
+++ app.py
@@ -0,0 +1,128 @@
+from flask import Flask, request, jsonify
+import matplotlib
+matplotlib.use('Agg')
+import os
+from datetime import datetime
+import pickle
+import gymnasium as gym
+import gymnasium_env
+import yaml
+
+
+app = Flask(__name__)
+
+with open('/home/dcv-user/zhli/ubiquant/game/cfg.yaml') as f:
+    main_cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+legal_team_id = []
+with open(main_cfg["team_id_path"]) as f:
+    for l in f.readlines():
+        legal_team_id.append(l.strip())
+print('team_id:', legal_team_id)
+
+
+def init_game(team_id, kwargs={}):
+    if not os.path.exists(os.path.join(main_cfg["save_dir"], team_id)):
+        os.mkdir(os.path.join(main_cfg["save_dir"], team_id))
+    now = datetime.now()
+    time_key = now.strftime("%Y%m%d-%H%M%S")
+    game_id = f'{team_id}_{time_key}'
+    game_dir = os.path.join(main_cfg["save_dir"], game_id.replace("_", "/"))
+    while True:
+        try:
+            os.makedirs(game_dir, exist_ok=False)
+        except OSError as e:
+            print(e)
+            game_id = game_id +'a'
+            game_dir = os.path.join(main_cfg["save_dir"], game_id.replace("_", "/"))
+        else:
+            print('makedirs', game_dir)
+            break
+    #print(datetime.now(), 'Begin game, ', kwargs)
+    game_env = gym.make('gymnasium_env/GAME', **kwargs)
+    obs, info = game_env.reset()
+    #print(datetime.now(), 'finish reset')
+    with open(f'{game_dir}/game_env.pkl', 'wb') as f:
+        pickle.dump(game_env, f)
+    #print(datetime.now(), 'finish dump game_env')
+    return obs['image'].tolist(), obs['bag'].tolist(), obs['grid'].tolist(), game_id
+
+
+def env_step(game_id, action, cls):
+    #print(datetime.now(), 'begin env_step')
+    game_dir = os.path.join(main_cfg["save_dir"], game_id.replace("_", "/"))
+    with open(f'{game_dir}/game_env.pkl', 'rb') as f:
+        game_env = pickle.load(f)
+    #print(datetime.now(), 'finish pickle load')
+    cls_penalty = 0
+    if (action == 4) and (game_env.get_current_cls() != -1):
+        correct = cls == game_env.get_current_cls()
+        if not correct:
+            cls_penalty = -0.1
+    obs, rew, term, _, info = game_env.step(action)
+    #print(datetime.now(), 'finish step')
+    rew += cls_penalty
+    with open(f'{game_dir}/game_env.pkl', 'wb') as f:
+        pickle.dump(game_env, f)
+    #print(datetime.now(), 'finish pickle dump')
+    if term:
+        with open(f'{game_dir}/finish.txt', 'w') as f:
+            f.write('finish')
+    #print(datetime.now(), 'end env_step')
+    return obs['bag'].tolist(), obs['grid'].tolist(), rew, term
+
+
+def process_team_post(data, team_id):
+    # check team id legal
+    if team_id in legal_team_id:
+        if data.get('begin', None):
+            param_type = data["begin"]
+            if param_type not in [1, 2, 3, 4]:
+                return f'begin must be in [1, 2, 3, 4], but found {param_type} with type({type(param_type)})', 400
+            params = main_cfg[f'param{param_type}']
+            print(f'Begin game type {data["begin"]}, team {team_id}')
+            img, bag, grid, game_id = init_game(team_id, params)
+            return jsonify({'is_end': False, 'img': img, 'bag':bag, 'score': 0, 'game_id': game_id, 'grid': grid})
+        else:
+            # continue existing game, game_id, action, cls
+            game_id = data.get('game_id', None)  # str
+            if game_id is None:
+                print('Game id not found')
+                return 'Game id not found', 400
+            print(f'Continue game, id: {game_id}')
+            game_dir = os.path.join(main_cfg["save_dir"], game_id.replace("_", "/"))
+            if os.path.exists(f'{game_dir}/finish.txt'):
+                print('Game has finished')
+                return 'Game has finished', 400
+            action = data.get('action', None)
+            if action is None:
+                print('Action cannot be None')
+                return 'Action cannot be None', 400
+            cls = data.get('cls', None)
+            if (action == 4) and (cls is None):
+                print('Cls data cannot be None')
+                return 'Cls data cannot be None', 401
+            bag, grid, score, is_end = env_step(game_id, action, cls)
+            result = {'is_end': is_end, 'bag': bag, 'score': score, 'game_id': game_id, 'grid': grid}
+            return jsonify(result)
+    else:
+        print(f'team_id:{team_id} illegal')
+        return "team_id illegal", 402
+
+
+@app.route('/', methods=['POST'])
+def handle_client():
+    try:
+        data = request.get_json()
+        team_id = data.get('team_id', None)
+        if team_id is not None:
+            return process_team_post(data, team_id)
+        else:
+            return "team_id is none", 403
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        return jsonify({'status': e, 'message': str(e)}), 500
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=8081)
