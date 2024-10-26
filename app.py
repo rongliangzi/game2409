c124d3128d6cc154435aa65c374a68bc7b5bf634
root <root@ip-172-31-14-133.cn-northwest-1.compute.internal>
2024-10-26 10:00:59 +0000

    clean up

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 from flask import Flask, request, jsonify
-import matplotlib
-matplotlib.use('Agg')
-import os
-from datetime import datetime
-import pickle
-import gymnasium as gym
-import gymnasium_env
 import yaml
 from rule_utils import *
 from game_utils import *
@@ -13,7 +6,7 @@ from game_utils import *
 
 app = Flask(__name__)
 
-with open('./cfg.yaml') as f:
+with open('./debug_cfg.yaml') as f:
     main_cfg = yaml.load(f, Loader=yaml.FullLoader)
 
 legal_team_id = []
@@ -28,53 +21,31 @@ def process_team_post(data, team_id):
     if data.get('begin', None):
         if not begin_if_can(team_id, main_cfg):
             return f'Team {team_id} has run out of game time {main_cfg["max_n"]}', 403
-        param_type = data["begin"]
-        if param_type not in [1, 2, 3, 4]:
-            return f'begin must be in [1, 2, 3, 4], but found {param_type} with type({type(param_type)})', 400
-        params = main_cfg[f'param{param_type}']
-        print(f'Begin game type {data["begin"]}, team {team_id}')
-        img, bag, grid, game_id = init_game(team_id, main_cfg, params)
-        game_dir = os.path.join(main_cfg["save_dir"], game_id.replace("_", "/"))
-        with open(f'{game_dir}/last_step_time.txt', 'w') as f:
-            f.write(datetime.now().strftime('%Y%m%d %H%M%S.%f'))
-        with open(f'{game_dir}/game_result.pkl', 'wb') as f:
-            pickle.dump({'cum_score': 0, 'game_type': param_type}, f)
+        if not check_begin(data['begin']):
+            return f'Illegal begin', 400
+        img, bag, grid, game_id = init_game(team_id, main_cfg, data['begin'])
         return jsonify({'is_end': False, 'img': img, 'bag':bag, 'score': 0, 'game_id': game_id, 'grid': grid})
     else:
         # continue existing game, game_id, action, cls
         game_id = data.get('game_id', None)  # str
+        if game_id is None:
+            return 'Game id must be provided', 400
         game_dir = os.path.join(main_cfg["save_dir"], game_id.replace("_", "/"))
+        if not os.path.exists(game_dir):
+            return 'Game id does not exist', 400
         with open(f'{game_dir}/last_step_time.txt', 'r') as f:
             last_step_time = datetime.strptime(f.readlines()[0].strip(), '%Y%m%d %H%M%S.%f')
         time_diff = (datetime.now() - last_step_time).total_seconds()
-        if game_id is None:
-            print('Game id not found')
-            return 'Game id not found', 400
-        #print(f'Continue game, id: {game_id}')
-        if os.path.exists(f'{game_dir}/finish.txt'):
-            print('Game has finished')
-            return 'Game has finished', 400
         action = data.get('action', None)
-        if action is None:
-            print('Action cannot be None')
-            return 'Action cannot be None', 400
         cls = data.get('cls', None)
-        if (action == 4) and (cls is None):
-            print('Cls data cannot be None')
-            return 'Cls data cannot be None', 401
-        bag, grid, score, is_end = env_step(game_id, main_cfg, action, cls)
-        if time_diff > float(main_cfg['max_step_seconds']):
-            print(f"{game_id}, time_diff:{time_diff:.6f}, max_step_seconds:{float(main_cfg['max_step_seconds']):.1f}")
-            score += main_cfg['timeout_penalty']
-        game_dir = os.path.join(main_cfg["save_dir"], game_id.replace("_", "/"))
-        with open(f'{game_dir}/game_result.pkl', 'rb') as f:
-            game_result = pickle.load(f)
-            game_result['cum_score'] += score
-        with open(f'{game_dir}/game_result.pkl', 'wb') as f:
-            pickle.dump(game_result, f)
+        grid_cls = data.get('grid_cls', None)
+        info, code = check_step_data(game_id, game_dir, action, cls, grid_cls)
+        if len(info) > 0:
+            return info, code
+        bag, grid, score, is_end = env_step(game_id, main_cfg, action, cls, grid_cls)
+        score += get_time_penalty(time_diff, main_cfg, game_id)
+        update_game_result(game_dir, score)
         result = {'is_end': is_end, 'bag': bag, 'score': score, 'game_id': game_id, 'grid': grid}
-        with open(f'{game_dir}/last_step_time.txt', 'w') as f:
-            f.write(datetime.now().strftime('%Y%m%d %H%M%S.%f'))
         return jsonify(result)
 
 
