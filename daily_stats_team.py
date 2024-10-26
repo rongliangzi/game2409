4f7dc0a4b3687d17e3c993230686dadf36429c67
root <root@ip-172-31-14-133.cn-northwest-1.compute.internal>
2024-10-26 10:01:15 +0000

    add columns

--- daily_stats_team.py
+++ daily_stats_team.py
@@ -7,26 +7,29 @@ import pickle
 
 
 if __name__=="__main__":
-    with open('./cfg.yaml') as f:
+    with open('./debug_cfg.yaml') as f:
         cfg = yaml.load(f, Loader=yaml.FullLoader)
-    yesterday = datetime.now() - timedelta(days=1)
+    yesterday = datetime.now()# - timedelta(days=1)
     yesterday = yesterday.strftime("%Y%m%d")
-    print(f'Stats leaderboard on {yesterday}')
+    print(f'Stats team score on {yesterday}')
     save_dir = cfg['save_dir']
     all_teams = os.listdir(save_dir)
-    day_stats = {'cum_score': [], 'game_type': []}
+    day_stats = {'cum_score': [], 'begin': [], 'rounds': [], 'acc': []}
     index = []
     for team_id in all_teams:
+        # iterate on all teams
         team_dir = os.path.join(save_dir, team_id)
         all_game_key = os.listdir(team_dir)
         for game_key in all_game_key:
+            # all games of one team
             if game_key.startswith(yesterday):
+                # games starting on yesterday
                 game_result_path = os.path.join(team_dir, game_key, 'game_result.pkl')
                 if not os.path.exists(game_result_path):
                     continue
                 with open(game_result_path, 'rb') as f:
                     game_result = pickle.load(f)
-                for k in day_stats.keys()
+                for k in day_stats.keys():
                     day_stats[k].append(game_result[k])
                 index.append((team_id, game_key))
     print(day_stats)
