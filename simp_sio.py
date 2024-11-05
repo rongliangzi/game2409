4c9e00f151eb9c333a2eaef05280a01de7222beb
root <root@ip-172-31-3-125.cn-northwest-1.compute.internal>
2024-11-05 10:39:03 +0000

    flask socketio

--- simp_sio.py
+++ simp_sio.py
@@ -1,27 +1,56 @@
-from flask import Flask
+from flask import Flask, request
 from flask_socketio import SocketIO, emit
 import random
 import numpy as np
+
+# request.sid: game_env
+sid_game = dict()
+# team_id: connect_num, restrict connect each team
+team_connect = dict()
 
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='eventlet')
 
+
 @socketio.on('continue')
 def handle_continue(msg):
-    print(f'Continue Received message: {msg}')
-    num = np.random.random()
-    emit('response', {'is_end': num>0.99, 'score': 0.1, 'bag': {1: 0}})
+    sid = request.sid
+    team_id = msg['team_id']
+    sid_game[sid]['rounds'] += 1
+    print(f'Continue {sid}', sid_game[sid]['rounds'])
+    is_end = sid_game[sid]['rounds'] > 576
+    if is_end:
+        team_connect[team_id]-=1
+    emit('response', {'team_id': team_id, 'game_id': msg['game_id'], 
+                      'is_end': is_end, 'score': 0.1, 'bag': {1: 0}, 'loc': (0,0)})
 
 
 @socketio.on('begin')
 def handle_begin(msg):
     print(f'Begin : {msg}')
-    num = np.random.random()
-    grid = np.random.randint(0, 21, size=(12, 12))
-    emit('response', {'is_end': num>0.999, 'grid': grid.tolist(), 'score': 0, 'bag': {0: 0}})
+    team_id = msg['team_id']
+    if team_connect.get(team_id, 0) >= 20:
+        emit('response', {'error': 'cannot begin because reaching max connections'})
+    else:
+        grid = np.random.randint(0, 21, size=(12, 12), dtype=int)
+        img = np.random.randint(0, 255, size=(600,600,3), dtype=np.uint8)
+        game_id = np.random.randint(0, 999999)
+        state = {'team_id': team_id, 'game_id': str(game_id), 
+                 'img': img.tolist(), 'grid': grid.tolist(), 'score': 0, 'bag': {0: 0}, 'rounds': 0, 'loc': (0,0)}
+        sid_game[request.sid] = state
+        team_connect[team_id] = team_connect.get(team_id, 0) + 1
+        emit('response', state)
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    try:
+        del sid_game[request.sid]
+        print('disconnect', request.sid, len(sid_game))
+    except Exception as e:
+        print('Exception', e)
 
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8081)
-
--- sio.py
+++ sio.py
@@ -1,54 +1,148 @@
 from flask import Flask, request
-from flask_socketio import SocketIO, emit, disconnect
-#import eventlet
+import eventlet
+from flask_socketio import SocketIO, emit
+import random
 import numpy as np
+import yaml
+import logging
+from rule_utils import *
+from game_utils import *
 
 
-# init Flask and SocketIO
+# request.sid: game_env
+sid_game = dict()
+# team_id: connect_num, restrict connect each team
+team_connect = dict()
+max_team_connect = 30
+# begin step
+begin_sid = []
+max_begin_num = 40
+
+
 app = Flask(__name__)
-socketio = SocketIO(app)
-connect_games = {}
+socketio = SocketIO(app, async_mode='eventlet', ping_timeout=60)
+logging.basicConfig(level=logging.INFO)
 
-init_game_state = {"score": 0, "position": [0, 0], 'grid': [[1,1,], [2,2]], 'bag': {1: 0}}
+with open('./cfg/debug_cfg.yaml') as f:
+    main_cfg = yaml.load(f, Loader=yaml.FullLoader)
 
-@socketio.on('connect')
-def handle_connect():
-    print("Client connected", request.sid)
+legal_team_id = read_team_id(main_cfg['team_id_path'])
 
-@socketio.on('init_game')
-def handle_init_game(data):
-    print('init game', data)
+
+def init_game2(team_id, main_cfg, begin):
+    # init a game from existing game_data
+    time_str = ''
+    st = time.time()
+    game_type = begin[0]
+    param_type = type_dir = game_type_dic.get(game_type, game_type)
+    game_data_id = begin[1:]
+    #print(f'Begin game type {game_type}, team {team_id}')
+    env_args = main_cfg[f'param{param_type}']
+    now = datetime.now()
+    time_key = now.strftime("%Y%m%d-%H%M%S")
+    time_str += f'{datetime.now()}, Begin game'
+    game_id, game_dir = get_game_id_dir(main_cfg, team_id, time_key)
+    for k in ['img_dir', 'cls_names']:
+        env_args[k] = main_cfg[k]
+    get_init_grid_loc(env_args, main_cfg, type_dir, game_data_id)
+    game_env = gym.make('gymnasium_env/GAME', **env_args)
+    obs, info = game_env.reset()
+    time_str += f'{datetime.now()}, finish reset'
+    if game_type in full_game_types:
+        grid = np.zeros_like(obs['grid'])
+    if time.time() - st > 1:
+        print(f'Env init too long time {game_id}\n', time_str)
+    return obs['image'].tolist(), obs['bag'].tolist(), grid.tolist(), obs['loc'].tolist(), game_id, game_env
+
+
+def env_step2(game_env, game_id, main_cfg, action):
+    # update env, send new data to client
+    time_str = ''
+    st = time.time()
+    time_str += f'{datetime.now()}, begin env_step\n'
+    game_dir = os.path.join(main_cfg["save_dir"], game_id.replace("_", "/"))
+    obs, rew, term, _, info = game_env.step(action)
+    time_str += f'{datetime.now()} finish step\n'
+    if term:
+        with open(f'{game_dir}/finish.txt', 'w') as f:
+            f.write('finish')
+    time_str += f'{datetime.now()} end env_step\n'
+    if time.time() - st > 1:
+        print(f'Env step too long time {game_id}\n', time_str)
+    return obs['bag'].tolist(), obs['loc'].tolist(), rew, term
+
+
+@socketio.on('continue')
+def handle_continue(data):
+    sid = request.sid
+    if sid in begin_sid:
+        begin_sid.remove(sid)
     team_id = data['team_id']
-    if team_id not in connect_games:
-        connect_games[team_id] = []
-    connect_games[team_id].append(request.sid)
-    print(connect_games)
-    socketio.emit('game_init', init_game_state)
+    game_id = data['game_id']
+    action = data['action']
+    sid_game[sid]['rounds'] += 1
+    if sid_game[sid]['rounds'] < 1:
+        print(f'Continue {sid}', sid_game[sid]['rounds'])
+    if sid_game[sid]['rounds'] == 0  and sid_game[sid]['game_data_id'][0] in ['2', '3', '4']:
+        grid_pred = data['grid_pred']
+        cls_label = sid_game[sid]['env'].unwrapped.get_init_grid()
+        sid_game[sid]['acc'] = (grid_pred == cls_label).sum() / cls_label.size
+    bag, loc, score, is_end = env_step2(sid_game[sid]['env'], game_id, main_cfg, action)
+    if is_end:
+        game_dir = os.path.join(main_cfg['save_dir'], game_id.replace('_', '/'))
+        with open(f'{game_dir}/game_result.pkl', 'wb') as f:
+            pickle.dump({'cum_score': 0, 'acc': 0, 'begin': 0, 'rounds': 0, 'time_itv': 0}, f)
+    emit('response', {'team_id': team_id, 'game_id': data['game_id'], 'rounds': sid_game[sid]['rounds'],
+                      'is_end': is_end, 'score': score, 'bag': bag, 'loc': loc})
 
-@socketio.on('action')
-def handle_action(data):
-    print("Received action from client:", data)
-    new_game_state = {
-            "score": init_game_state["score"] + 1,
-            "position": init_game_state['position'],
-            'grid': init_game_state['grid'],
-            'bag': init_game_state['bag']
-            }
-    if np.random.random() < 0.2:
-        print(data['team_id'], 'game over')
-        socketio.emit('game_over', new_game_state)
-        disconnect()
+
+@socketio.on('begin')
+def handle_begin(data):
+    print(f'Begin : {data}')
+    team_id = data['team_id']
+    if team_id not in legal_team_id:
+        emit('response', {'error': 'Illegal team_id'})
+    elif team_connect.get(team_id, 0) >= max_team_connect:
+        emit('response', {'error': 'cannot begin because reaching max connections'})
     else:
-        socketio.emit('game_update', new_game_state)
+        try:
+            begin_num = sum(v['rounds']==0 for v in sid_game.values()) if len(sid_game) > 0 else 0
+            while begin_num >= max_begin_num:
+                eventlet.sleep(2)
+                begin_num = sum(v['rounds']==0 for v in sid_game.values())
+                for v in sid_game.values():
+                    if v['rounds'] == 0:
+                        print(v['team_id'], v['game_id'], 'rounds=0')
+                print(request.sid, len(sid_game), begin_num, len(begin_sid))
+                #print('waiting for begin emit')
+            begin_sid.append(request.sid)
+            os.makedirs(os.path.join(main_cfg["save_dir"], team_id), exist_ok=True)
+            img, bag, grid, loc, game_id, env = init_game2(team_id, main_cfg, data['begin'])
+            game_info = {'team_id': team_id, 'game_id': game_id, 'env': env, 'game_data_id': data['begin'],
+                         'rounds': 0, 'lasttime': 0, 'interval': []}
+            sid_game[request.sid] = game_info
+            team_connect[team_id] = team_connect.get(team_id, 0) + 1
+            emit('response', {'img': img, 'grid': grid, 'score': 0, 'bag': bag, 'loc': loc, 'game_id': game_id, 'team_id': team_id, 'rounds': game_info['rounds']})
+        except Exception as e:
+            print(f'{sid} exception: {e}')
+            disconnect()
+
+@socketio.event
+def error_handler(e):
+    logging.error(f"ERROR occur: {e}")
+    disconnect()
 
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    try:
-        print('disconnect', request.sid)
-    except Exception as e:
-        print('Exception', e)
+    if request.sid in sid_game:
+        team_id = sid_game[request.sid]['team_id']
+        team_connect[team_id] -= 1
+        del sid_game[request.sid]
+        print(f'team: {team_id}, sid {request.sid} disconnect, current team connect: {team_connect[team_id]}')
+    else:
+        logging.warning(f'Unknown or already disconnect client: {request.sid}')
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=8081)
+    socketio.run(app, host='0.0.0.0', port=8081)
